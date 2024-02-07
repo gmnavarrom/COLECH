1b786e6a7bcf50ec866ceb6ee25e77f040846964
Javier Alejandro Aguilera Navarro <aaguilera@apisoft.com.mx>
2024-02-07 10:41:46 -0700

    Add project files.

---
+++ colech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace colech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
